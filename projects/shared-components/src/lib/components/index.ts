8938a29c86217df2e3554b572db6e70820665c44
Prakash Subramaniam <210319@usthealthproof.com>
2025-07-29 19:43:42 +0530

    UP-363 | Added checkbox and progress spinner components

--- projects/shared-components/src/lib/components/index.ts
+++ projects/shared-components/src/lib/components/index.ts
@@ -1,2 +1,4 @@
 export * from './button';
 export * from './dropdown';
+export * from './checkbox';
+export * from './loader';
---
+++ projects/shared-components/src/lib/components/loader/progress-spinner/index.ts
@@ -0,0 +1,1 @@
+export { ProgressSpinnerComponent as HpxProgressSpinner } from './progress-spinner.component';
